19b62ee0f4884bb74b07cddc758b0dcf8a8e24ca
Jyasveer Gotta <jyasveer@gmail.com>
2017-10-21 23:44:18 +0530

    scopes, local, global

--- 3-functions/1-local-and-global.js
+++ 3-functions/1-local-and-global.js
@@ -1,8 +1,12 @@
+var test = 'random';
+
 function run() {
     var temp = '123';
     temp2 = '345';
 }
 
+run();
+console.log(window)
 var temp3 = '';
 run();
 console.log('temp2 = ', temp2);// temp2 is created without using the 'var' keyword. so it is availabel globally
@@ -10,5 +14,5 @@ console.log('temp2 = ', temp2);// temp2 is created without using the 'var' keywo
 
 // NOTE :- ALWAYS USE 'var' KEYWORD WHEN YOU DECLARE A VARIABLE
 
-console.log(global.temp2); // temp2 gets created in the window scope
-console.log(global.temp);
+console.log(window.temp2); // temp2 gets created in the window scope
+console.log(window.temp);
