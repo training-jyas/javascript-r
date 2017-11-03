06953d9d2fb8060aaab5ad9b8961bcb08b24db9b
Jyasveer Gotta <jyasveer@gmail.com>
2017-11-04 00:09:49 +0530

    functions continued - iife, hoisting, closures

--- 3-functions/6-closures.js
+++ 3-functions/6-closures.js
@@ -1,16 +1,25 @@
+// what is closure ?
+// Closure gives you a way to access a scope although you are out of the scope execution cycle
+
 var closureFunc = function (num) {
-    var innerFunc = function(num2){
-        var innerfunc2 = function(num3){
-            return num + num2 + num3;
-        }
-        return innerfunc2;
-    }
+    var innerFunc = function() {
+        console.log(num);
+    };
+    innerFunc();
     return innerFunc;
 };
 
-var func = closureFunc(10);
-var func2 = func(20);
-console.log("value from inner func 2 : ",func2(30));
+var innerFunc = closureFunc(30);
+console.log('inner func : ', innerFunc);
+console.log('inner func called : ', innerFunc());
 
-// what is closure ?
-// Closure gives you a way to access scope although you are out of the scope execution cycle
+// example
+// sum method using closures
+function sum(num1) {
+    return function (num2) {
+        return function (num3) {
+            return num1 + num2 + num3;
+        }
+    }
+}
+console.log(sum(10)(20)(30));
