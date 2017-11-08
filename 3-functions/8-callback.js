c45a84a01535b9577355a3cf9d5cd3f40bea336c
Jyasveer Gotta <jyasveer@gmail.com>
2017-11-08 23:35:25 +0530

    recursive and callback and module done

--- 3-functions/8-callback.js
+++ 3-functions/8-callback.js
@@ -1,4 +1,5 @@
 // example to show if the parameter is a function and how to check
+
 // callbacks
 var succesCallback = function (num) {
     console.log('i am the SUCCESS callback. and the num is :', num);
@@ -15,14 +16,12 @@ var checkIf20 = function (num, success, failure) {
         } else {
             console.log("num is :", num);
         }
-        return true;
     } else {
         if (typeof failure === 'function') {
             failure(num);
         } else {
             console.log("num is not 20 but :", num);
         }
-        return false;
     }
 }
 
@@ -34,3 +33,24 @@ console.log("######################");
 console.log('if the number is 20 and callback executed -', checkIf20(20, succesCallback, failureCallback));
 console.log("######################");
 console.log('if the number is 20 and callback executed -', checkIf20(30, succesCallback, failureCallback));
+
+
+// for example let us try to understand the each method in jquery
+var each = function(array, callback) {
+    if (array && typeof array === 'object' && array.length) {
+        var length = array.length;
+        for (var i = 0; i < length; i++) {
+            if (typeof callback === 'function') {
+                callback(i, array[i]);
+            }
+        }
+    }
+};
+var jquery = {
+    each: each
+};
+
+var array = [1, 2, 3];
+jquery.each(array, function(index, item) {
+    console.log(index, ' = ', item);
+});
--- 3-functions/assignments.js
+++ 3-functions/assignments.js
@@ -2,25 +2,35 @@
 // create a pricing module
 // this will accept a item object and it will give you the price for it in rupees or dollars depending on the parameter
 // price object should have mrp price, discounted price, and discount percentage
+
 // create a availability module
 // pass the item object, it should give me the availablity in boolean.
+
 // create a canDeliver module
 // given the item is available if it can be delivered to a specific pin code
 
 // You have to use all the concepts form iife, closures, modules, callback, typeof
 
 // Data models
-var item = {
-    "name": '',
-    "color": '',
-    "type": ''
-};
-
 var price = {
     "mrp": '',
     "disPrice": '',
     "disc": ''
-}
+};
 
-var picodes = ['', '', '', ''];
+var item = {
+    "name": '',
+    "color": '',
+    "type": '',
+    "price": price
+};
+
+
+var pincodes = ['', '', '', ''];
 var itemsInStock = [{}, {}, {}, {}];
+
+// 1. searchAItem()
+// 2. addItem()
+// 3. getPriceOfaItem()
+// 4. isItemInStock()
+// 5. canDeliver()
